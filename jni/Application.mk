f6cf2900404fec584f792926ba6e0c49a90367ab
Benjamin Eikel <benjamin@eikel.org>
2013-11-14 14:16:36 +0100

    Adapt build to new directory structure

--- jni/Application.mk
+++ jni/Application.mk
@@ -13,4 +13,4 @@
 APP_STL := gnustl_shared
 APP_CPPFLAGS := -fexceptions -frtti -std=c++11
 NDK_TOOLCHAIN_VERSION := 4.8
-NDK_MODULE_PATH := $(call my-dir)/ext
+NDK_MODULE_PATH := $(call my-dir)/ThirdParty
